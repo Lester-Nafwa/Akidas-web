bcd8a0595c151968f3e88beaf1f8ddba8693ac1c
Lester-Nafwa <nafwalester@gmail.com>
2023-08-09 16:42:41 +0300

    update error

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,22 @@ import Home from "./pages/Home/home";
 import About from "./pages/About/about";
 import Products from "./pages/Products/product";
 import Contact from "./pages/Contacts/contacts";
+import Error from "./pages/error/error";
 
 function App() {
   return (
     <div>
       <div className="icon-prof">
-        <div className="bg-head"> 
-        <img
-          src={process.env.PUBLIC_URL + "/assets/profo.png"}
-          className="img-prof"
-          alt=""
-        />
-        <hr />
-        <div className="title-sub">
-          <span>Pharmaceutical & Medical Supplies</span>
-        </div>
+        <div className="bg-head">
+          <img
+            src={process.env.PUBLIC_URL + "/assets/profo.png"}
+            className="img-prof"
+            alt=""
+          />
+          <hr />
+          <div className="title-sub">
+            <span>Pharmaceutical & Medical Supplies</span>
+          </div>
         </div>
         <Nav />
       </div>
@@ -30,6 +31,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/product" element={<Products />} />
           <Route path="/contacts" element={<Contact />} />
+          <Route path="*" element={<Error />} />
         </Routes>
       </div>
     </div>
---
+++ src/pages/error/error.jsx
@@ -0,0 +1,10 @@
+import "./error.css";
+
+const Error = () => {
+  return (
+    <div>
+      <div className="error-page"> Page Not Available</div>
+    </div>
+  );
+};
+export default Error;
