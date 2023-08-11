061d182dc4457bb56384bd9708d99bae111ebd7e
Lester-Nafwa <nafwalester@gmail.com>
2023-08-12 02:19:52 +0300

    update css

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -57,78 +57,81 @@ const Home = () => {
         </div>
       </div>
       <footer className="footer">
-        <p>Contact us at: </p>
-        <div><img src="/assets/contactus.png" alt=""/></div>
-        <div className="socia-handle">
-          <a href="https://www.facebook.com/profile.php?id=100087974205941">
-            <div className="icons-social">
-              <div>
-                <img
-                  src={process.env.PUBLIC_URL + "/assets/facebook.png"}
-                  className="icon-img"
-                  alt=""
-                />
+        <div className="details-contacts">
+          <div className="img-banner"><img src="/assets/contactus.png" alt="" className="banner-contact" /></div>
+          <div  >
+            <div  className="socia-handle" >
+            <a href="https://www.facebook.com/profile.php?id=100087974205941">
+              <div className="icons-social">
+                <div>
+                  <img
+                    src={process.env.PUBLIC_URL + "/assets/facebook.png"}
+                    className="icon-img"
+                    alt=""
+                  />
+                </div>
+                <span>Facebook</span>
               </div>
-              <span>Facebook</span>
-            </div>
-          </a>
+            </a>
 
-          <a href="https://www.instagram.com/ikadaskenya/">
-            <div className="icons-social">
-              <div>
-                <img
-                  src={process.env.PUBLIC_URL + "/assets/instagram.png"}
-                  className="icon-img"
-                  alt=""
-                />
+            <a href="https://www.instagram.com/ikadaskenya/">
+              <div className="icons-social">
+                <div>
+                  <img
+                    src={process.env.PUBLIC_URL + "/assets/instagram.png"}
+                    className="icon-img"
+                    alt=""
+                  />
+                </div>
+                <div>
+                  {" "}
+                  <span>Instagram</span>
+                </div>
               </div>
-              <div>
-                {" "}
-                <span>Instagram</span>
+            </a>
+            <a href="https://www.linkedin.com/company/ikadas/">
+              <div className="icons-social">
+                <div>
+                  <img
+                    src={process.env.PUBLIC_URL + "/assets/linked.pnd.png"}
+                    className="icon-img"
+                    alt=""
+                  />
+                </div>
+                <div>
+                  {" "}
+                  <span>LinkedIn</span>
+                </div>
               </div>
+            </a>
             </div>
-          </a>
-          <a href="https://www.linkedin.com/company/ikadas/">
-            <div className="icons-social">
-              <div>
-                <img
-                  src={process.env.PUBLIC_URL + "/assets/linked.pnd.png"}
-                  className="icon-img"
-                  alt=""
-                />
+            <div className="contact-list">
+              <div className="iconics">
+                <div>  <img src='/assets/telephon-icon.svg' alt="" className="contact-icon" /> </div>
+                <div>  <span>+254 707064823</span></div>
               </div>
-              <div>
-                {" "}
-                <span>LinkedIn</span>
-              </div>
-            </div>
-          </a>
-        </div>
-        <div className="contact-list">
-          <div className="iconics">
-         <div>  <img src='/assets/telephon-icon.svg' alt="" className="contact-icon" /> </div> 
-          <div>  <span>+254 707064823</span></div> 
-          </div>
-          <br />
-          <div className="iconics">
-         <div> <img src='/assets/email-icon.svg' alt="" className="contact-icon"/> </div> 
-          <div>  <span>ismahan.adawe@gmail.com</span></div> 
-          </div>
-          <br />
-          <div className="locate-ico">
-            <div>
-              {" "}
-               <img src='/assets/location.svg' alt="" className="contact-icon"/> 
-            </div>
-            <div>
-              <div>
-                {" "}
-                <span>Nairobi, Kenya</span>
-              </div>{" "}
               <br />
-              <div>
-                {" "}
-                <span>Mogadishu, Somalia</span>
+              <div className="iconics">
+                <div> <img src='/assets/email-icon.svg' alt="" className="contact-icon" /> </div>
+                <div>  <span>ismahan.adawe@gmail.com</span></div>
+              </div>
+              <br />
+              <div className="locate-ico">
+                <div>
+                  {" "}
+                  <img src='/assets/location.svg' alt="" className="contact-icon" />
+                </div>
+                <div>
+                  <div>
+                    {" "}
+                    <span>Nairobi, Kenya</span>
+                  </div>{" "}
+                  <br />
+                  <div>
+                    {" "}
+                    <span>Mogadishu, Somalia</span>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
